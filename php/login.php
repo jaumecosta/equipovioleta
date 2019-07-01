87ef125734fd4d67cf3580c2dea4592380689f5c
David Gavilanes <davidgavilanesvelez@gmail.com>
2019-07-01 16:24:07 +0200

    cambios en el php

--- php/login.php
+++ php/login.php
@@ -3,20 +3,24 @@
 function login(){
 
 $nombre = $_POST['nombre'];
-$password = $_POST['pass'];
+$password = $_POST['password'];
 $email = $_POST['email'];
-$verificado = $_POST['verificado'];
+if($_POST['verificado'] == "on"){
+    $verificado = true;
+}else{
+    $verificado = false;
+}
 
 $con = new Conexion('localhost','root','','Signup&in'); 
 
-$query = $con->query('SELECT pass,email,verificado FROM signup');
+$query = $con->query('SELECT password,email,verificado FROM signup');
 
 while($fila = $query->fetch_assoc()){
 
 
 if($fila['email'] == $email && $fila['pass'] == $pass){
 
- echo header("location: CAMBIARPAGINADEDESTINO");   
+ echo header("location: xd.html");   
 
 }else if($fila['email'] != $email || $fila['pass'] != $pass){
 
--- php/signin.php
+++ php/signin.php
@@ -5,10 +5,13 @@ require_once '../models/User.php';
 
 $hay_usuario = false;
 $nombre = $_POST['nombre'];
-$password = $_POST['pass'];
+$password = $_POST['password'];
 $email = $_POST['email'];
-$verificado = $_POST['verificado'];
-
+if($_POST['verificado'] == "on"){
+    $verificado = true;
+}else{
+    $verificado = false;
+}
 $con = new Conexion('localhost','root','','Signup&in');
 
 $c = $con->Query('SELECT nombre,password,email,verificado FROM singin');
