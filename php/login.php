ade727b6f4b4e9e55453e7b2b7ae0c077b752f44
David Gavilanes <davidgavilanesvelez@gmail.com>
2019-07-01 15:18:46 +0200

    creacion del login

--- php/login.php
+++ php/login.php
@@ -1,11 +1,32 @@
 <?php
 
-
+function login(){
 
 $nombre = $_POST['nombre'];
-$password = $_POST['nombre'];
-$email = $_POST['nombre'];
-$verificado = $_POST['nombre'];
+$password = $_POST['pass'];
+$email = $_POST['email'];
+$verificado = $_POST['verificado'];
 
-$c = new Conexion('localhost','root','','Signup&in');
+$con = new Conexion('localhost','root','','Signup&in'); 
 
+$query = $con->Query('SELECT pass,email,verificado FROM signup');
+
+while($fila = $query->fetch_assoc()){
+
+
+if($fila['email'] == $email && $fila['pass'] == $pass){
+
+ echo header("location: CAMBIARPAGINADEDESTINO");   
+
+}else if($fila['email'] != $email || $fila['pass'] != $pass){
+
+echo 'Email o contraseña incorrectos';
+
+}
+
+
+
+}
+
+
+}
